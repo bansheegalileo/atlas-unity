c2bdb45708ee20ee53444423ef0dbed0acde83f8
bansheegalileo <venable.rin@gmail.com>
2024-02-28 19:24:48 -0600

    here

---
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 8f;
+    public float gravity = 20f;
+    public float resetYPosition = -14.9f;
+
+    private CharacterController characterController;
+    private Vector3 moveDirection;
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalMovement, 0f, verticalMovement);
+        movement = transform.TransformDirection(movement);
+        movement *= moveSpeed * Time.deltaTime;
+
+        if (characterController.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        characterController.Move(movement + moveDirection * Time.deltaTime);
+
+        if (transform.position.y < resetYPosition)
+        {
+            ResetPlayer();
+        }
+    }
+
+    public void ResetPlayer()
+    {
+        transform.position = startPosition;
+        moveDirection = Vector3.zero;
+    }
+}
